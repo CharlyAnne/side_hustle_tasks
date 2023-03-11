6f8c7cc856ce96acdf3d9bd3ee573081a11b8f76
CharlyAnne <icharlieanne@gmail.com>
2023-03-11 03:43:30 +0100

    files upload

---
+++ Task 1/app.js
@@ -0,0 +1,9 @@
+function validate(form) {
+    if(!valid) {
+        alert('Please correct the errors in the form!');
+        return false;
+    }
+    else {
+        return confirm('Do you really want to submit the form?');
+    }
+}
